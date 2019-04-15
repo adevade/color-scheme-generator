38f03ddbb2443d7af391d6ead293455d9f5dc51a
Andréas Lundgren <adevade@gmail.com>
2019-04-15 19:46:13 +0200

    Copy index.html from src to root

--- webpack.mix.js
+++ webpack.mix.js
@@ -3,6 +3,7 @@ let tailwindcss = require('tailwindcss');
 require('laravel-mix-purgecss');
 
 mix.setPublicPath('dist/')
+    .copy('src/views/index.html', 'index.html')
     .js('src/js/app.js', 'dist/')
     .postCss('src/css/app.css', 'dist/', [tailwindcss()])
     .purgeCss({folders: ['src']});
