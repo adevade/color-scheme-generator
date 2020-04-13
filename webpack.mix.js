ec3d67147710c3607d773a51c04847ccc01ac17a
Andréas Lundgren <adevade@gmail.com>
2020-04-13 14:13:49 +0200

    Update dependencies and formatting

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,8 +1,15 @@
-let mix = require('laravel-mix');
-require('laravel-mix-purgecss');
+let mix = require("laravel-mix");
 
-mix.setPublicPath('dist/')
-    .copy('src/views/index.html', 'index.html')
-    .js('src/js/app.js', 'dist/')
-    .postCss('src/css/app.css', 'dist/', [require('tailwindcss')])
-    .purgeCss({folders: ['src']});
+let purgecss = require("@fullhuman/postcss-purgecss")({
+  content: ["src/**/*.html", "src/**/*.js"],
+  defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
+});
+
+mix
+  .copy("src/views/index.html", "index.html")
+  .js("src/js/app.js", "dist/")
+  .postCss("src/css/app.css", "dist/", [
+    require("tailwindcss"),
+    require("autoprefixer"),
+    ...(process.env.NODE_ENV === "production" ? [purgecss] : [])
+  ]);
