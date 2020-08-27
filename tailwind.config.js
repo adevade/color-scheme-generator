fcc6316158cee18b0b012f33167dee42a7a54a6f
Andréas Lundgren <adevade@gmail.com>
2020-08-27 23:16:33 +0200

    Update dependencies

---
+++ tailwind.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  purge: ["./src/**/*.html", "./src/**/*.js"],
+  future: {
+    removeDeprecatedGapUtilities: true,
+  },
+};
--- webpack.mix.js
+++ webpack.mix.js
@@ -1,15 +1,9 @@
 let mix = require("laravel-mix");
 
-let purgecss = require("@fullhuman/postcss-purgecss")({
-  content: ["src/**/*.html", "src/**/*.js"],
-  defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
-});
-
 mix
   .copy("src/views/index.html", "index.html")
   .js("src/js/app.js", "dist/")
   .postCss("src/css/app.css", "dist/", [
     require("tailwindcss"),
     require("autoprefixer"),
-    ...(process.env.NODE_ENV === "production" ? [purgecss] : [])
   ]);
