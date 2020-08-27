4bea68f5d5b0aafe9f6b554a7f04a0b00ff1adcd
Andréas Lundgren <adevade@gmail.com>
2020-08-27 23:17:01 +0200

    Formatting

--- src/js/app.js
+++ src/js/app.js
@@ -16,21 +16,21 @@ new Vue({
     tabs: [
       {
         id: "tailwind",
-        title: "Tailwind"
+        title: "Tailwind",
       },
       {
         id: "sass",
-        title: "SASS"
+        title: "SASS",
       },
       {
         id: "scss",
-        title: "SCSS"
+        title: "SCSS",
       },
       {
         id: "less",
-        title: "Less"
-      }
-    ]
+        title: "Less",
+      },
+    ],
   },
 
   computed: {
@@ -44,28 +44,28 @@ new Vue({
       return {
         brand: {
           name: "Brand",
-          value: this.brand
+          value: this.brand,
         },
         cta: {
           name: "CTA",
-          value: this.brand.set("hsl.h", "+150")
+          value: this.brand.set("hsl.h", "+150"),
         },
         info: {
           name: "Info",
-          value: chroma.mix("#3df", this.brand, 0.2, "lab")
+          value: chroma.mix("#3df", this.brand, 0.2, "lab"),
         },
         warning: {
           name: "Warning",
-          value: chroma.mix("#fd0", this.brand, 0.2, "lab")
+          value: chroma.mix("#fd0", this.brand, 0.2, "lab"),
         },
         success: {
           name: "Success",
-          value: chroma.mix("#3e4", this.brand, 0.2, "lab")
+          value: chroma.mix("#3e4", this.brand, 0.2, "lab"),
         },
         danger: {
           name: "Danger",
-          value: chroma.mix("#f34", this.brand, 0.2, "lab")
-        }
+          value: chroma.mix("#f34", this.brand, 0.2, "lab"),
+        },
       };
     },
 
@@ -73,42 +73,42 @@ new Vue({
       return {
         white: {
           name: "White",
-          value: chroma("#fff")
+          value: chroma("#fff"),
         },
         "gray-lightest": {
           name: "gray Lightest",
-          value: chroma.mix("#fafafa", this.brand, 0.01, "lab")
+          value: chroma.mix("#fafafa", this.brand, 0.01, "lab"),
         },
         "gray-lighter": {
           name: "gray Lighter",
-          value: chroma.mix("#e6e6e6", this.brand, 0.01, "lab")
+          value: chroma.mix("#e6e6e6", this.brand, 0.01, "lab"),
         },
         "gray-light": {
           name: "gray Light",
-          value: chroma.mix("#d2d2d2", this.brand, 0.01, "lab")
+          value: chroma.mix("#d2d2d2", this.brand, 0.01, "lab"),
         },
         gray: {
           name: "gray",
-          value: chroma.mix("#bfbfbf", this.brand, 0.01, "lab")
+          value: chroma.mix("#bfbfbf", this.brand, 0.01, "lab"),
         },
         "gray-dark": {
           name: "gray Dark",
-          value: chroma.mix("#979797", this.brand, 0.01, "lab")
+          value: chroma.mix("#979797", this.brand, 0.01, "lab"),
         },
         "gray-darker": {
           name: "gray Darker",
-          value: chroma.mix("#6f6f6f", this.brand, 0.01, "lab")
+          value: chroma.mix("#6f6f6f", this.brand, 0.01, "lab"),
         },
         "gray-darkest": {
           name: "gray Darkest",
-          value: chroma.mix("#484848", this.brand, 0.01, "lab")
+          value: chroma.mix("#484848", this.brand, 0.01, "lab"),
         },
         black: {
           name: "Black",
-          value: chroma.mix("#202020", this.brand, 0.01, "lab")
-        }
+          value: chroma.mix("#202020", this.brand, 0.01, "lab"),
+        },
       };
-    }
+    },
   },
 
   methods: {
@@ -127,6 +127,6 @@ new Vue({
 
     shade(hex) {
       return chroma.mix("#000", hex, 0.5, "lab");
-    }
-  }
+    },
+  },
 });
