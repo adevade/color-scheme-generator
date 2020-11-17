64b6efcefb16976048c03d8c0b284f3e4e373073
Andréas Lundgren <adevade@gmail.com>
2020-11-17 16:15:33 +0100

    Update dependencies

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 module.exports = {
   purge: ["./src/**/*.html", "./src/**/*.js"],
   future: {
+    defaultLineHeights: true,
+    purgeLayersByDefault: true,
     removeDeprecatedGapUtilities: true,
+    standardFontWeights: true,
   },
 };
