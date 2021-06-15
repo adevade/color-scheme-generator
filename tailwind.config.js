032f43c527861244e33f3c7f40e0ff6b2f496e10
Andréas Lundgren <adevade@gmail.com>
2021-06-15 14:32:25 +0200

    wip

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,4 @@
 module.exports = {
+  mode: "jit",
   purge: ["./src/**/*.html", "./src/**/*.js"],
-  future: {
-    defaultLineHeights: true,
-    purgeLayersByDefault: true,
-    removeDeprecatedGapUtilities: true,
-    standardFontWeights: true,
-  },
 };
--- webpack.mix.js
+++ webpack.mix.js
@@ -2,8 +2,7 @@ let mix = require("laravel-mix");
 
 mix
   .copy("src/views/index.html", "index.html")
-  .js("src/js/app.js", "dist/")
   .postCss("src/css/app.css", "dist/", [
-    require("tailwindcss"),
     require("autoprefixer"),
+    require("tailwindcss"),
   ]);
